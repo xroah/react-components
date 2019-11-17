9795d7cd5803424567ada95abe2fea01a054e5ee
xroah <396069583@qq.com>
2019-11-17 17:14:26 +0800

    popover

---
+++ src/Popover.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Overlay, { CommonProps } from "./Overlay";
+import { getStyle } from "./Tooltip";
+import { classNames } from "./utils";
+
+export interface PopoverProps extends CommonProps {
+    title?: string;
+    content: React.ReactNode;
+}
+
+export default function Popover(props: PopoverProps) {
+    const {
+        title,
+        children,
+        placement = "right",
+        content,
+        offset,
+        ...otherProps
+    } = props;
+    const popup = (
+        <div className={
+            classNames(
+                "popover",
+                `bs-popover-${placement}`
+            )
+        }>
+            <div className="arrow" style={getStyle(placement)} />
+            {
+                !!title && (
+                    <div className="popover-header">{title}</div>
+                )
+            }
+            <div className="popover-body">
+                {content}
+            </div>
+        </div>
+    );
+    return (
+        <Overlay
+            unmountOnclose
+            alignmentPrefix="bs-popover"
+            align="center"
+            offset={offset}
+            placement={placement}
+            popup={popup}
+            clearMargin={false}
+            verticalCenter
+            {...otherProps}>
+            {children}
+        </Overlay>
+    );
+
+}
+
+Popover.propTypes = {
+    content: PropTypes.node.isRequired
+};
+Popover.defaultProps = {
+    trigger: ["click"]
+};
