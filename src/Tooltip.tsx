dbf8dce113ffeac27113093a4f1faa6bccc9e093
xroah <396069583@qq.com>
2019-11-17 17:14:03 +0800

    functional

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -4,69 +4,70 @@ import Overlay, { CommonProps } from './Overlay';
 import { classNames } from "./utils";
 
 export interface TooltipProps extends CommonProps {
-    
+
 }
 
-export default class Tooltip extends React.Component<TooltipProps> {
-
-    static defaultProps = {
-        trigger: ["hover"]
+export function getStyle(placement: any) {
+    const leftStyle = {
+        transform: "translateX(-50%)",
+        left: "50%"
     };
-    static propTypes = {
-        title: PropTypes.node.isRequired
+    const topStyle = {
+        transform: "translateY(-50%)",
+        top: "50%"
+    };
+    const posMap: any = {
+        top: leftStyle,
+        bottom: leftStyle,
+        left: topStyle,
+        right: topStyle
     };
 
-    render() {
-        const {
-            title,
-            children,
-            placement = "top",
-            className,
-            offset,
-            ...otherProps
-        } = this.props;
-        const leftStyle = {
-            transform: "translateX(-50%)",
-            left: "50%"
-        };
-        const topStyle = {
-            transform: "translateY(-50%)",
-            top: "50%"
-        };
-        const posMap: any = {
-            top: leftStyle,
-            bottom: leftStyle,
-            left: topStyle,
-            right: topStyle
-        };
-        const popup = (
-            <div className={
-                classNames(
-                    "show",
-                    "tooltip",
-                    `bs-tooltip-${placement}`
-                )
-            }>
-                <div className="arrow" style={{ ...posMap[placement] }} />
-                <div className="tooltip-inner">
-                    {title}
-                </div>
+    return posMap[placement];
+}
+
+export default function Tooltip(props: TooltipProps) {
+    const {
+        title,
+        children,
+        placement = "top",
+        offset,
+        ...otherProps
+    } = props;
+
+    const popup = (
+        <div className={
+            classNames(
+                "show",
+                "tooltip",
+                `bs-tooltip-${placement}`
+            )
+        }>
+            <div className="arrow" style={getStyle(placement)} />
+            <div className="tooltip-inner">
+                {title}
             </div>
-        );
+        </div>
+    );
 
-        return (
-            <Overlay
-                placement={placement}
-                popup={popup}
-                align="center"
-                offset={offset}
-                alignmentPrefix="bs-tooltip"
-                unmountOnclose
-                verticalCenter
-                {...otherProps}>
-                {children}
-            </Overlay>
-        );
-    }
-
+    return (
+        <Overlay
+            placement={placement}
+            popup={popup}
+            align="center"
+            offset={offset}
+            alignmentPrefix="bs-tooltip"
+            unmountOnclose
+            verticalCenter
+            {...otherProps}>
+            {children}
+        </Overlay>
+    );
 }
+
+Tooltip.defaultProps = {
+    trigger: ["hover"]
+};
+Tooltip.propTypes = {
+    title: PropTypes.node.isRequired
+};
