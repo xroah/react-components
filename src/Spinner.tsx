3a027a6e6d28ca5720053a271c3767eaab3f1e85
xroah <396069583@qq.com>
2019-11-21 19:53:31 +0800

    spinner

---
+++ src/Spinner.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import {
+    classNames,
+    variantArray,
+    variantType
+} from "./utils";
+
+export interface SpinnerProps extends React.HTMLAttributes<HTMLElement> {
+    variant?: variantType;
+    animation: "border" | "grow";
+    size?: "sm";
+}
+
+export default function Spinner(props: SpinnerProps) {
+    const {
+        variant,
+        size,
+        animation,
+        className,
+        children,
+        ...otherProps
+    } = props;
+    const _className = `spinner-${animation}`;
+
+    return (
+        <div className={
+            classNames(
+                className,
+                _className,
+                size && `${_className}-${size}`,
+                variant && `text-${variant}`
+            )
+        } {...otherProps}>
+            {children}
+        </div>
+    );
+}
+
+Spinner.propTypes = {
+    size: PropTypes.oneOf(["sm"]),
+    animation: PropTypes.oneOf(["border", "grow"]).isRequired,
+    variant: PropTypes.oneOf(variantArray)
+};
