b66aa7bad09da7b16caf235ec716c38729c76a48
xroah <396069583@qq.com>
2019-11-21 20:14:25 +0800

    jumbotron

---
+++ src/Jumbotron.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { classNames } from "./utils";
+
+export interface JumbotronProps extends React.HTMLAttributes<HTMLElement> {
+    fluid?: boolean;
+}
+
+export default function Jumbotron(props: JumbotronProps) {
+    const {
+        className,
+        fluid,
+        ...otherProps
+    } = props;
+
+
+    return <div className={
+        classNames(
+            className,
+            "jumbotron",
+            fluid && "jumbotron-fluid"
+        )
+    } {...otherProps}/>;
+}
+
+Jumbotron.propTypes = {
+    fluid: PropTypes.bool
+};
