c8d476b9480fd7f7434c62e98f2d13703134d977
xroah <396069583@qq.com>
2019-12-15 21:46:58 +0800

    Col

--- components/basic/Col.tsx
+++ components/basic/Col.tsx
@@ -21,6 +21,61 @@ export interface ColProps extends React.HTMLAttributes<HTMLElement> {
     xl?: sizeType;
 }
 
-export default class Col extends React.Component {
+const sizeProp = PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.number,
+    PropTypes.oneOf(["auto"]),
+    PropTypes.shape({
+        span: PropTypes.oneOfType([
+            PropTypes.oneOf(["auto"]),
+            PropTypes.number
+        ]),
+        order: PropTypes.number,
+        offset: PropTypes.number
+    })
+]);
+export default class Col extends React.Component<ColProps> {
+
+    static propTypes = {
+        tag: PropTypes.elementType,
+        span: PropTypes.oneOfType([
+            PropTypes.oneOf(["auto"]),
+            PropTypes.bool,
+            PropTypes.number
+        ]),
+        offset: PropTypes.number,
+        order: PropTypes.number,
+        sm: sizeProp,
+        md: sizeProp,
+        lg: sizeProp,
+        xl: sizeProp
+    };
+    static defaultProps = {
+        tag: "div"
+    };
+
+    handleSize(obj: SizeObject, type: "sm" | "md" | "lg" | "xl" | "col") {
+
+    }
+
+    render() {
+        const {
+            tag,
+            span,
+            offset,
+            sm,
+            md,
+            lg,
+            xl,
+            order,
+            className,
+            ...otherProps
+        } = this.props;
+        
+
+        return React.createElement(
+            tag as React.ElementType
+        );
+    }
 
 }
