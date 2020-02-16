7eddb83e26e3fb1cb2f809c0a0e84e8a8801b5c9
xroah <396069583@qq.com>
2020-02-16 09:42:07 +0800

    revise implementation

--- components/basic/Media.tsx
+++ components/basic/Media.tsx
@@ -4,6 +4,7 @@ import { classNames } from "../utils";
 
 export interface MediaProps extends React.HTMLAttributes<HTMLElement> {
     img?: string | React.ReactNode;
+    alt?: string;
     header?: string | React.ReactNode;
     imgSize?: number;
     imgRounded?: boolean;
@@ -23,6 +24,7 @@ export default function Media(props: MediaProps) {
         imgCircle,
         alignment,
         imgPosition,
+        alt,
         ...otherProps
     } = props;
     const alignmentMap: any = {
@@ -31,56 +33,67 @@ export default function Media(props: MediaProps) {
         bottom: "align-self-end"
     };
     let _img = img;
-    let _header = header;
-    let leftImg: any;
-    let rightImg: any;
+    const imgClasses = classNames(
+        imgCircle && "rounded-circle",
+        imgRounded && "rounded",
+        alignmentMap[alignment as string],
+        imgPosition === "right" ? "ml-3" : "mr-3"
+    );
 
-    if (img && typeof img === "string") {
-        const imgClasses = classNames(
-            imgCircle && "rounded-circle",
-            imgRounded && "rounded",
-            alignmentMap[alignment as string],
-            imgPosition === "right" ? "ml-3" : "mr-3"
-        );
+    if (typeof img === "string") {
         _img = (
             <img
                 src={img}
+                alt={alt}
                 className={imgClasses}
                 width={imgSize}
                 height={imgSize} />
         );
+    } else if (React.isValidElement(img)) {
+        _img = React.cloneElement<any>(
+            img,
+            {
+                className: classNames(
+                    (img.props as any).className,
+                    imgClasses
+                ),
+                alt,
+                width: imgSize,
+                height: imgSize
+            }
+        );
     }
 
-    if (imgPosition === "right") {
-        rightImg = _img;
-    } else {
-        leftImg = _img;
-    }
-
-    if (header && typeof header === "string") {
-        _header = <h5 className="mt-0">{header}</h5>;
-    }
+    const body = (
+        <div className="media-body">
+            {
+                header && (
+                    <h5 className="mt-0 mb-1">{header}</h5>
+                )
+            }
+            {children}
+        </div>
+    );
 
     return (
         <div
             className={classNames(className, "media")}
             {...otherProps}>
-            {leftImg}
-            <div className="media-body">
-                {_header}
-                {children}
-            </div>
-            {rightImg}
+            {
+                imgPosition === "right" ?
+                    <>{body}{_img}</> :
+                    <>{_img}{body}</>
+            }
         </div>
     );
 }
 
 Media.defaultProps = {
-    imgSize: 60
+    imgSize: 64
 };
 Media.propTypes = {
     img: PropTypes.oneOfType([PropTypes.bool, PropTypes.node]),
-    header:PropTypes.oneOfType([PropTypes.bool, PropTypes.node]),
+    header: PropTypes.oneOfType([PropTypes.bool, PropTypes.node]),
     imgSize: PropTypes.number,
     imgRounded: PropTypes.bool,
     imgCircle: PropTypes.bool,
