f0931f3c6edcb5baadc073cee0e300ae55ee19af
xroah <396069583@qq.com>
2020-03-05 15:28:14 +0800

    getScrollParent

--- components/utils.ts
+++ components/utils.ts
@@ -228,6 +228,34 @@ export function getScrollBarWidth() {
     return width;
 }
 
+export function getScrollParent(el: HTMLElement, until: HTMLElement = document.body) {
+    let parent: HTMLElement = el;
+
+    while ((parent = parent.parentNode as HTMLElement) && until !== parent) {
+        const w = parent.clientWidth;
+        const h = parent.clientHeight;
+        const sw = parent.scrollWidth;
+        const sh = parent.scrollHeight;
+        const style = getComputedStyle(parent);
+        const overflow = style.getPropertyValue("overflow");
+        const overflowX = style.getPropertyValue("overflow-x")
+        const overflowY = style.getPropertyValue("overflow-y");
+
+        if (
+            (w < sw || h < sh) &&
+            (
+                overflow !== "visible" ||
+                overflowX !== "visible" ||
+                overflowY !== "visible"
+            )
+        ) {
+            return parent;
+        }
+    }
+
+    return until;
+}
+
 export type variantType = "primary"
     | "secondary"
     | "success"
