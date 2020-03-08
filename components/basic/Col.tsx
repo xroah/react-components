848a9c46f238b7c0f5fa5b572720419da9c21d3a
xroah <396069583@qq.com>
2020-03-08 15:39:55 +0800

    fix span bugs

--- components/basic/Col.tsx
+++ components/basic/Col.tsx
@@ -82,7 +82,7 @@ export default class Col extends React.Component<ColProps> {
         return [
             this.handleOffsetOrOrder(`offset-${type}`, val.offset),
             this.handleOffsetOrOrder(`order-${type}`, val.order),
-            this.handleSpan(prefix, val.span)
+            this.handleSpan(prefix, val.span === undefined ? true : val.span)
         ];
     }
 
