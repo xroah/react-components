3eca8326ba42c69c624e0c388167e7aae53ac525
xroah <396069583@qq.com>
2020-03-31 10:47:34 +0800

    move equalWidth from Item to ListGroup and add ListGroupContext

--- components/ListGroup/ListGroup.tsx
+++ components/ListGroup/ListGroup.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import { classNames } from "../utils";
 import { CommonProps } from "../CommonPropsInterface";
+import { ListGroupContext } from "../contexts";
 
 export interface ListGroupProps extends CommonProps<HTMLDivElement> {
     flush?: boolean;
     minWidth?: "sm" | "md" | "lg" | "xl";
     horizontal?: boolean;
+    equalWidth?: boolean;
 }
 
 export default function ListGroup(props: ListGroupProps) {
@@ -15,28 +17,33 @@ export default function ListGroup(props: ListGroupProps) {
         minWidth,
         horizontal,
         className,
+        equalWidth,
         ...otherProps
     } = props;
     const PREFIX = "list-group";
     let H_PREFIX = `${PREFIX}-horizontal`;
 
     return (
-        <div className={
-            classNames(
-                PREFIX,
-                flush && `${PREFIX}-flush`,
-                horizontal && minWidth ? `${H_PREFIX}-${minWidth}` : horizontal ? H_PREFIX : ""
-            )
-        } {...otherProps}/>
+        <ListGroupContext.Provider value={!!equalWidth}>
+            <div className={
+                classNames(
+                    PREFIX,
+                    flush && `${PREFIX}-flush`,
+                    horizontal && minWidth ? `${H_PREFIX}-${minWidth}` : horizontal ? H_PREFIX : ""
+                )
+            } {...otherProps} />
+        </ListGroupContext.Provider>
     );
 }
 
 ListGroup.propTypes = {
     flush: PropTypes.bool,
     minWidth: PropTypes.oneOf(["sm", "md", "lg", "xl"]),
-    horizontal: PropTypes.bool
+    horizontal: PropTypes.bool,
+    equalWidth: PropTypes.bool
 };
 ListGroup.defaultProps = {
     horizontal: false,
-    flush: false
+    flush: false,
+    equalWidth: false
 };
