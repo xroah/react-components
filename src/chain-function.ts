3c2b09b67ebc502f7b6981ac52661efb37fe67c5
xroah <396069583@qq.com>
2021-02-08 20:11:36 +0800

    bug fix

--- src/chain-function.ts
+++ src/chain-function.ts
@@ -1,13 +1,12 @@
 export default (...fn: any[]) => {
     return fn.reduce(
-        (acc, cur) => (args: any[]) => {
-            if (typeof acc === "function") {
-                acc.apply(null, args)
-            }
+        (acc, cur) => (...args: any[]) => {
+            acc.apply(null, args)
 
             if (typeof cur === "function") {
                 cur.apply(null, args)
             }
-        }
+        },
+        () => {}
     )
 }
