6e9ba47609a46e082b148434ad6bf17da2f88f74
xroah <396069583@qq.com>
2021-03-23 20:54:36 +0800

    misc

--- demo/components/fade-demo.tsx
+++ demo/components/fade-demo.tsx
@@ -6,14 +6,12 @@ export default function () {
     const handleClick = () => update(!visible)
 
     return (
-        <div>
+        <>
             <p>Fade demo</p>
-            <div>
-                <button onClick={handleClick}>toggle</button>
-            </div>
+            <button onClick={handleClick}>toggle</button>
             <Fade in={visible} timeout={300}>
                 <div className="div">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, labore maiores expedita at magnam perspiciatis consequuntur architecto voluptas quo quos eum repellendus repellat rerum officia accusamus odio nesciunt consectetur. Dignissimos.</div>
             </Fade>
-        </div>
+        </>
     )
 }
