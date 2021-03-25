cf7bd60b799aa421995c4ed75fa6f48e3de06d99
xroah <396069583@qq.com>
2021-03-25 09:38:54 +0800

    bug fix

--- src/dom/get-scroll-parent.ts
+++ src/dom/get-scroll-parent.ts
@@ -3,24 +3,19 @@ export default (el: HTMLElement, until: HTMLElement = document.body) => {
     let ret: HTMLElement | null = null
 
     while ((parent = parent.parentNode as HTMLElement) && until !== parent) {
-        const w = parent.clientWidth
-        const h = parent.clientHeight
-        const sw = parent.scrollWidth
-        const sh = parent.scrollHeight
         const style = getComputedStyle(parent)
         const overflow = style.getPropertyValue("overflow")
         const overflowX = style.getPropertyValue("overflow-x")
         const overflowY = style.getPropertyValue("overflow-y")
 
         if (
-            (w < sw || h < sh) &&
-            (
-                overflow !== "visible" ||
-                overflowX !== "visible" ||
-                overflowY !== "visible"
-            )
+            overflow !== "visible" ||
+            overflowX !== "visible" ||
+            overflowY !== "visible"
         ) {
             ret = parent
+
+            break
         }
     }
 
@@ -28,5 +23,5 @@ export default (el: HTMLElement, until: HTMLElement = document.body) => {
         ret = until
     }
 
-    return until
+    return ret
 }
