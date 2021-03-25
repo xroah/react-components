94f5ae5737cb2f86d6691ea0b284dd0f9440aae5
xroah <396069583@qq.com>
2021-03-25 09:39:01 +0800

    test

---
+++ tests/spec/get-scroll-parent.spec.ts
@@ -0,0 +1,28 @@
+import getScrollParent from "../../src/dom/get-scroll-parent"
+
+it("Should get scroll parent", () => {
+    const html = `
+        <div id="scrollParent" style="overflow: auto">
+            <div>
+                <div id="element"></div>
+            </div>
+        </div>
+    `
+    document.body.innerHTML = html
+    const el = document.getElementById("element")
+    const parent = document.getElementById("scrollParent")
+
+    expect(getScrollParent(el)).toBe(parent)
+})
+
+it("Should get body", () => {
+    const html = `
+        <div id="scrollParent">
+            <div id="element"></div>
+        </div>
+    `
+    document.body.innerHTML = html
+    const el = document.getElementById("element")
+
+    expect(getScrollParent(el)).toBe(document.body)
+})
