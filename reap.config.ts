c00e80cbf5174398395ce64c5c8c9cbc9e1d977f
xroah <396069583@qq.com>
2021-05-20 20:05:13 +0800

    cdn

--- reap.config.ts
+++ reap.config.ts
@@ -1,21 +1,28 @@
-const path = require("path")
+import {resolve} from "path"
+
+const isDev = process.env.NODE_ENV === "development"
 
 export default {
     webpack: {
         entry: "./docs/index.tsx",
         resolve: {
             alias: {
-                "reap-ui$": path.resolve("./components/index.ts")
+                "reap-ui$": resolve("./components/index.ts")
             }
         },
-        externals: {
+        externals: isDev ? undefined : {
             "react": "React",
             "react-dom": "ReactDOM",
             "react-router-dom": "ReactRouterDOM"
         }
     },
     htmlWebpackPlugin: {
-        favicon: "./public/logo.png"
+        favicon: "./public/logo.png",
+        cdn: isDev ? "" : `
+            <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+            <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+            <script crossorigin src="https://unpkg.com/react-router-dom@5.2.0/umd/react-router-dom.min.js"></script>
+        `
     },
     devServer: {
         port: 8000,
