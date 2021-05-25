879a5afcf99fc27573804eef42a1947c44f24d6e
xroah <396069583@qq.com>
2021-05-25 19:46:20 +0800

    update examples

--- docs/examples/Button/index.tsx
+++ docs/examples/Button/index.tsx
@@ -3,11 +3,14 @@ import Button from "reap-ui/Button"
 
 export default () => {
     return (
-        <>
+        <div className="btn-examples">
             <Button>Primary</Button>
             <Button variant="link">Link</Button>
             <Button tag="a" href="#">Link</Button>
             <Button tag="input" variant="danger" type="reset" value="reset"/>
-        </>
+            <Button outline>Outline primary</Button>
+            <Button size="lg">Large button</Button>
+            <Button size="sm">Small button</Button>
+        </div>
     )
 }
