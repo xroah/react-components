7f4de1c5e9f803869986915b6eded2c6963ec74c
xroah <396069583@qq.com>
2021-05-26 18:31:08 +0800

    update

--- docs/examples/Button/index.tsx
+++ docs/examples/Button/index.tsx
@@ -2,15 +2,28 @@ import * as React from "react"
 import Button from "reap-ui/Button"
 
 export default () => {
+    const [active, toggle] = React.useState(true)
+    const handleClick = () => {
+        toggle(!active)
+    }
+
     return (
         <div className="btn-examples">
-            <Button>Primary</Button>
-            <Button variant="link">Link</Button>
-            <Button tag="a" href="#">Link</Button>
-            <Button tag="input" variant="danger" type="reset" value="reset"/>
-            <Button outline>Outline primary</Button>
-            <Button size="lg">Large button</Button>
-            <Button size="sm">Small button</Button>
+            <div>
+                <Button>Primary</Button>
+                <Button variant="link">Link</Button>
+                <Button tag="a" href="#">Link</Button>
+                <Button tag="input" variant="danger" type="reset" value="reset" />
+                <Button outline>Outline primary</Button>
+                <Button size="lg">Large button</Button>
+                <Button size="sm">Small button</Button>
+            </div>
+            <div>
+                <Button.Toggle href="#">Toggle link</Button.Toggle>
+                <Button.Toggle active={active} onClick={handleClick}>
+                    Toggle button
+                </Button.Toggle>
+            </div>
         </div>
     )
 }
