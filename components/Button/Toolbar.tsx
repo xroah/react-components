9751d78c7713d1162b9c2d5ff01f874f420f8b2c
xroah <396069583@qq.com>
2021-05-28 22:45:38 +0800

    Button toolbar

---
+++ components/Button/Toolbar.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import classNames from "reap-utils/lib/class-names"
+
+export default function Toolbar(
+    {
+        className,
+        ...restProps
+    }: React.HTMLAttributes<HTMLDivElement>
+) {
+    const classes = classNames(className, "btn-toolbar")
+
+    return (
+        <div
+            role="toolbar"
+            className={classes}
+            {...restProps} />
+    )
+}
--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,15 +1,18 @@
 import Button from "./Button"
 import Toggle from "./Toggle"
 import Group from "./Group"
+import Toolbar from "./Toolbar"
 
 interface ButtonType {
     Toggle: typeof Toggle
-    Group: typeof Group
+    Group: typeof Group,
+    Toolbar: typeof Toolbar
 }
 
 const _Button = Button as (ButtonType & typeof Button)
 
 _Button.Toggle = Toggle
 _Button.Group = Group
+_Button.Toolbar = Toolbar
 
 export default _Button
