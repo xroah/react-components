96fb0e14276bf0bac107b48fe8833e55291ac283
xroah <396069583@qq.com>
2021-05-28 21:39:07 +0800

    move ButtonGroup

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,12 +1,15 @@
 import Button from "./Button"
 import Toggle from "./Toggle"
+import Group from "./Group"
 
 interface ButtonType {
     Toggle: typeof Toggle
+    Group: typeof Group
 }
 
 const _Button = Button as (ButtonType & typeof Button)
 
 _Button.Toggle = Toggle
+_Button.Group = Group
 
 export default _Button
