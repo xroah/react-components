b4f63090689ac70f8b3bdfc625a8a400cc71d127
xroah <396069583@qq.com>
2021-05-28 20:57:11 +0800

    ButtonGroup

---
+++ components/ButtonGroup/index.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+import classNames from "reap-utils/lib/class-names"
+import PropTypes from "prop-types"
+
+interface GroupProps extends React.HTMLAttributes<HTMLDivElement> {
+    size?: "sm" | "lg"
+    vertical?: boolean
+}
+
+export default function ButtonGroup(
+    {
+        size,
+        vertical,
+        className,
+        ...restProps
+    }: GroupProps
+) {
+    const PREFIX = "btn-group"
+    const classes = classNames(
+        className,
+        PREFIX,
+        size && `${PREFIX}-${size}`,
+        vertical && `${PREFIX}-vertical`
+    )
+
+    return <div className={classes} {...restProps} />
+}
+
+ButtonGroup.defaultProps = {
+    vertical: false
+}
+ButtonGroup.propTypes = {
+    vertical: PropTypes.bool,
+    size: PropTypes.oneOf(["sm", "lg"])
+}
