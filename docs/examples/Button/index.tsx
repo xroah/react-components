a742b37f71d8a628908154542e071959bf7832ae
xroah <396069583@qq.com>
2021-05-28 21:57:45 +0800

    add button group examples

--- docs/examples/Button/index.tsx
+++ docs/examples/Button/index.tsx
@@ -8,25 +8,59 @@ export default () => {
     }
 
     return (
-        <div className="btn-examples">
-            <div>
-                <Button>Primary</Button>
-                <Button variant="link">Link</Button>
-                <Button tag="a" href="#">Link</Button>
-                <Button tag="input" variant="danger" type="reset" value="reset" />
-                <Button outline>Outline primary</Button>
-                <Button size="lg">Large button</Button>
-                <Button size="sm">Small button</Button>
+        <>
+            <div className="btn-examples">
+                <div>
+                    <Button>Primary</Button>
+                    <Button variant="link">Link</Button>
+                    <Button tag="a" href="#">Link</Button>
+                    <Button tag="input" variant="danger" type="reset" value="reset" />
+                    <Button outline>Outline primary</Button>
+                    <Button size="lg">Large button</Button>
+                    <Button size="sm">Small button</Button>
+                </div>
+                <div>
+                    <Button.Toggle href="#">Toggle link</Button.Toggle>
+                    <Button.Toggle active={active} onClick={handleClick}>
+                        Toggle button
+                    </Button.Toggle>
+                </div>
+                <div className="mb-2">
+                    <Button type="checkbox">Checkbox</Button>
+                </div>
             </div>
-            <div>
-                <Button.Toggle href="#">Toggle link</Button.Toggle>
-                <Button.Toggle active={active} onClick={handleClick}>
-                    Toggle button
-                </Button.Toggle>
+            <Button.Group className="me-2">
+                <Button>Left</Button>
+                <Button active>Middle</Button>
+                <Button>Right</Button>
+            </Button.Group>
+            <Button.Group size="lg" className="me-2">
+                <Button variant="danger">Left</Button>
+                <Button variant="warning">Middle</Button>
+                <Button variant="success">Right</Button>
+            </Button.Group>
+            <Button.Group className="me-2">
+                <Button outline>Left</Button>
+                <Button outline>Middle</Button>
+                <Button outline>Right</Button>
+            </Button.Group >
+            <Button.Group className="me-2">
+                <Button type="checkbox" outline>Left</Button>
+                <Button type="checkbox" outline>Middle</Button>
+                <Button type="checkbox" outline>Right</Button>
+            </Button.Group>
+            <Button.Group className="me-2">
+                <Button type="radio" name="radioBtn" outline>Left</Button>
+                <Button type="radio" name="radioBtn" outline>Middle</Button>
+                <Button type="radio" name="radioBtn" outline>Right</Button>
+            </Button.Group>
+            <div className="ms-2">
+                <Button.Group vertical>
+                    <Button variant="dark">Left</Button>
+                    <Button variant="dark">Middle</Button>
+                    <Button variant="dark">Right</Button>
+                </Button.Group>
             </div>
-            <div>
-                <Button type="checkbox">Checkbox</Button>
-            </div>
-        </div>
+        </>
     )
 }
