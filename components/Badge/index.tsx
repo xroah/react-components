5995d92aa5b47b5127d65bdf3a0bd35f0c4d045c
xroah <396069583@qq.com>
2021-06-04 21:08:46 +0800

    positioned badge and badge indicator

--- components/Badge/index.tsx
+++ components/Badge/index.tsx
@@ -2,11 +2,12 @@ import * as React from "react"
 import classNames from "reap-utils/lib/class-names"
 import PropTypes from "prop-types"
 import {Variant, Variants} from "../Commons/Variants"
+import omit from "reap-utils/lib/omit"
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLSpanElement> {
     variant?: Variant
     pill?: boolean
-    color?: "light" | "dark"
+    textColor?: "light" | "dark"
 }
 
 export default function Badge(props: BadgeProps) {
@@ -14,7 +15,7 @@ export default function Badge(props: BadgeProps) {
         className,
         variant,
         pill,
-        color,
+        textColor,
         ...restProps
     } = props
 
@@ -26,12 +27,75 @@ export default function Badge(props: BadgeProps) {
                     "badge",
                     variant && `bg-${variant}`,
                     pill && "rounded-pill",
-                    color && `text-${color}`
+                    textColor && `text-${textColor}`
                 )
             }
             {...restProps} />
     )
 }
+
+Badge.Positioned = function Positioned(
+    {
+        className,
+        pill = true,
+        variant = "danger",
+        ...restProps
+    }: BadgeProps
+) {
+    const classes = classNames(
+        className,
+        "position-absolute",
+        "top-0",
+        "start-100",
+        "translate-middle"
+    )
+
+    return (
+        <Badge
+            className={classes}
+            pill={true}
+            variant={variant}
+            {...restProps}
+        />
+    )
+}
+
+interface IndicatorProps extends React.HTMLAttributes<HTMLElement> {
+    size?: number
+    variant?: Variant
+}
+
+function Indicator({
+    className,
+    size,
+    style = {},
+    ...restProps
+}: IndicatorProps) {
+    const classes = classNames(
+        className,
+        "position-absolute",
+        "top-0",
+        "start-100",
+        "translate-middle",
+        "border",
+        "border-light",
+        "rounded-circle",
+        "p-0"
+    )
+
+    style.width = style.height = size
+
+    return (
+        <Badge
+            className={classes}
+            style={style}
+            {...omit(restProps, "children")}>
+            <span className="visually-hidden"></span>
+        </Badge>
+    )
+}
+
+Badge.Indicator = Indicator
 
 Badge.propTypes = {
     variant: PropTypes.oneOf(Variants),
@@ -40,4 +104,13 @@ Badge.propTypes = {
 }
 Badge.defaultProps = {
     pill: false
+}
+
+Indicator.propTypes = {
+    size: PropTypes.number,
+    variant: PropTypes.oneOf(Variants)
+}
+Indicator.defaultProps = {
+    size: 16,
+    variant: "danger"
 }
