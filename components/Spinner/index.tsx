c4081465172a380734e704b59e3bd50c88d5a6b5
xroah <396069583@qq.com>
2021-06-27 19:38:21 +0800

    Spinner

---
+++ components/Spinner/index.tsx
@@ -0,0 +1,48 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import {Variant} from "../Commons/Variants"
+import omit from "reap-utils/lib/omit"
+import classNames from "reap-utils/lib/class-names"
+
+interface SpinnerProps extends React.HTMLAttributes<HTMLSpanElement> {
+    variant?: Variant
+    type?: "border" | "grow"
+    size?: "sm" | number
+}
+
+export default function Spinner(
+    {
+        type,
+        variant,
+        className,
+        size,
+        style={},
+        ...restProps
+    }: SpinnerProps
+) {
+    const PREFIX = "spinner"
+    const sm = size === "sm"
+    const typeClass = `${PREFIX}-${type}`
+    const classes = classNames(
+        className,
+        typeClass,
+        type && `text-${variant}`,
+        sm && `${typeClass}-sm`
+    )
+
+    if (!sm) {
+        style.width = size
+        style.height = size
+    }
+
+    return (
+        <div
+            className={classes}
+            style={style}
+            {...omit(restProps, "children")} />
+    )
+}
+
+Spinner.defaultProps = {
+    type: "border"
+}
