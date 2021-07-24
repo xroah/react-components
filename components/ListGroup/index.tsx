e079a3e9114274efb4c9d821aecca95058fa1392
xroah <396069583@qq.com>
2021-07-24 19:08:49 +0800

    add Item to ListGroup

--- components/ListGroup/index.tsx
+++ components/ListGroup/index.tsx
@@ -1,58 +1,12 @@
-import * as React from "react"
-import warning from "warning"
-import classNames from "reap-utils/lib/class-names"
-import PropTypes from "prop-types"
+import ListGroup from "./ListGroup"
+import Item from "./Item"
 
-interface ListGroupProps extends
-    React.OlHTMLAttributes<HTMLOListElement | HTMLUListElement> {
-        flush?: boolean
-        numbered?: boolean
-        horizontal?: boolean
-        tag?: React.ElementType
+interface ListGroupType {
+    Item: typeof Item
 }
 
-export default function ListGroup({
-    flush,
-    numbered,
-    horizontal,
-    tag,
-    ...restProps
-}: ListGroupProps) {
-    const PREFIX = "list-group"
-    const prefix = (s: string) => `${PREFIX}-${s}`
-    const classes = classNames(
-        PREFIX,
-        flush && prefix("flush"),
-        numbered && prefix("numbered"),
-        horizontal && prefix("horizontal")
-    )
+const _ListGroup = ListGroup as (ListGroupType & typeof ListGroup)
 
-    if (tag === undefined) {
-        if (numbered) {
-            tag = "ol"
-        } else {
-            tag = "ul"
-        }
-    } else {
-        warning(
-            !(tag !== "ol" && numbered),
-            `The tag should be 'ol' if the list group is numbered,
-            received '${tag}'`
-        )
-    }
+_ListGroup.Item = Item
 
-    return React.createElement(
-        tag,
-        {
-            className: classes,
-            ...restProps
-        }
-    )
-}
-
-ListGroup.propTypes = {
-    flush: PropTypes.bool,
-    numbered: PropTypes.bool,
-    horizontal: PropTypes.bool,
-    tag: PropTypes.elementType
-}
+export default _ListGroup
