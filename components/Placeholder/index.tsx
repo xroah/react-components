2b65162b6ed610f1088cf4cb7049eddc4abc2d61
xroah <396069583@qq.com>
2021-07-27 21:04:08 +0800

    Placeholder

---
+++ components/Placeholder/index.tsx
@@ -0,0 +1,49 @@
+import * as React from "react"
+import classNames from "reap-utils/lib/class-names"
+import PropTypes from "prop-types"
+import {Variant, variants} from "../Commons/variants"
+import {getPrefixFunc} from "../Commons/utils"
+
+interface PlaceholderProps extends React.HTMLAttributes<HTMLDivElement> {
+    width?: string | number
+    variant?: Variant
+    size?: "xs" | "sm" | "lg"
+    animation?: "grow" | "wave"
+}
+
+export default function Placeholder(
+    {
+        width,
+        variant,
+        size,
+        animation,
+        className,
+        children,
+        ...restProps
+    }: PlaceholderProps
+) {
+    const prefix = getPrefixFunc("placeholder")
+    const classes = classNames(
+        className,
+        prefix(),
+        variant && `bg-${variant}`,
+        size && prefix(size),
+    )
+    const placeholder = <span className={classes}/>
+
+    if (animation) {
+        const Wrapper = (props: React.HTMLAttributes<HTMLDivElement>) => (
+            <div
+                className={prefix(animation)}
+                {...restProps}
+                {...props} />
+        )
+        if (children) {
+            return <Wrapper>{children}</Wrapper>
+        }
+
+        return <Wrapper>{placeholder}</Wrapper>
+    }
+
+    return React.cloneElement(placeholder, restProps)
+}
