8c4fec10e3dcdb42f109b64e8d359074216dde78
xroah <396069583@qq.com>
2021-08-06 21:30:08 +0800

    CardTitle

---
+++ components/Card/Title.tsx
@@ -0,0 +1,51 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import classNames from "reap-utils/lib/class-names"
+import {Variant} from "../Commons/variants"
+import {Color} from "./Card"
+import {isValidNode} from "../Commons/utils"
+
+interface CardTitleProps extends React.HTMLAttributes<HTMLDivElement> {
+    subTitle?: React.ReactNode
+    subTitleColor?: Color
+    color?: Color
+}
+
+export default function CardTitle(
+    {
+        subTitle,
+        subTitleColor,
+        color,
+        children,
+        ...restProps
+    }: CardTitleProps
+) {
+    return (
+        <div {...restProps}>
+            <h5 className={
+                classNames(
+                    "card-title",
+                    color && `text-${color}`
+                )
+            }>
+                {children}
+            </h5>
+            {
+                isValidNode(subTitle) && (
+                    <h6 className={
+                        classNames(
+                            "card-title",
+                            subTitleColor && `text-${subTitleColor}`
+                        )
+                    }>
+                        {subTitle}
+                    </h6>
+                )
+            }
+        </div>
+    )
+}
+
+CardTitle.defaultProps = {
+    subTitleColor: "muted"
+}
