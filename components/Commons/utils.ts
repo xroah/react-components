c8d1c7a2b38b96567c355f2c9ce594b5a0339c4b
xroah <396069583@qq.com>
2021-08-06 21:03:03 +0800

    isValidNode

--- components/Commons/utils.ts
+++ components/Commons/utils.ts
@@ -1,3 +1,9 @@
 export function getPrefixFunc(prefix: string) {
     return (s?: string) => s ? `${prefix}-${s}` : prefix
 }
+
+export function isValidNode(node: any) {
+    return node !== null &&
+        node !== undefined &&
+        typeof node !== "boolean"
+}
