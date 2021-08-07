6ad605d6691c113af3f0b79ce7d9b40926831167
xroah <396069583@qq.com>
2021-08-07 18:46:34 +0800

    card link

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,12 +1,29 @@
+import * as React from "react"
+import classNames from "reap-utils/lib/class-names"
 import Card from "./Card"
 import Body from "./Body"
 import Title from "./Title"
 import Text from "./Text"
 
+function Link(
+    {
+        className,
+        ...restProps
+    }: React.AnchorHTMLAttributes<HTMLAnchorElement>
+) {
+    return (
+        <a 
+        className={classNames(className, "card-link")}
+        {...restProps}
+        />
+    )
+}
+
 interface CardType {
     Body: typeof Body
     Title: typeof Title
     Text: typeof Text
+    Link: typeof Link
 }
 
 const _Card = Card as (CardType & typeof Card)
@@ -14,5 +31,6 @@ const _Card = Card as (CardType & typeof Card)
 _Card.Body = Body
 _Card.Title = Title
 _Card.Text = Text
+_Card.Link = Link
 
 export default _Card
