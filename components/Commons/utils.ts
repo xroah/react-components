73a82aecbb9e9eb3060c39c46c30d6301872f766
xroah <396069583@qq.com>
2021-08-09 19:20:26 +0800

    createComponent

--- components/Commons/utils.ts
+++ components/Commons/utils.ts
@@ -1,3 +1,6 @@
+import {createElement, ElementType, HTMLAttributes} from "react"
+import classNames from "reap-utils/lib/class-names"
+
 export function getPrefixFunc(prefix: string) {
     return (s?: string) => s ? `${prefix}-${s}` : prefix
 }
@@ -6,4 +9,38 @@ export function isValidNode(node: any) {
     return node !== null &&
         node !== undefined &&
         typeof node !== "boolean"
+}
+
+interface CreateOptions {
+    className?: string
+    tag?: ElementType
+    displayName?: string
+}
+
+export function createComponent<T extends HTMLAttributes<HTMLElement>>(
+    {
+        className,
+        tag="div",
+        displayName
+    }: CreateOptions,
+
+) {
+    const Component = ({
+        className: c,
+        ...restProps
+    }: T) => {
+        return createElement(
+            tag,
+            {
+                className: classNames(className, c),
+                ...restProps
+            }
+        )
+    }
+
+    if (displayName) {
+        Component.displayName = displayName
+    }
+
+    return Component
 }
