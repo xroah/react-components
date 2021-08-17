1daea661b75c9ad1e194edb00c69c8148025b238
xroah <396069583@qq.com>
2021-08-17 19:25:11 +0800

    import specified types from 'prop-types'

--- src/react/transition/propTeyps.ts
+++ src/react/transition/propTeyps.ts
@@ -1,18 +1,21 @@
-import PropTypes from "prop-types"
+import {
+    bool,
+    number,
+    func,
+    element,
+    oneOfType
+} from "prop-types"
 
 export default {
-    in: PropTypes.bool,
-    timeout: PropTypes.number,
-    unmountOnExit: PropTypes.bool,
-    appear: PropTypes.bool,
-    children: PropTypes.oneOfType([
-        PropTypes.func,
-        PropTypes.element
-    ]).isRequired,
-    onEnter: PropTypes.func,
-    onEntering: PropTypes.func,
-    onEntered: PropTypes.func,
-    onExit: PropTypes.func,
-    onExiting: PropTypes.func,
-    onExited: PropTypes.func,
+    in: bool,
+    timeout: number,
+    unmountOnExit: bool,
+    appear: bool,
+    children: oneOfType([func, element]).isRequired,
+    onEnter: func,
+    onEntering: func,
+    onEntered: func,
+    onExit: func,
+    onExiting: func,
+    onExited: func,
 }
