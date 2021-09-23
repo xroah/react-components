c2a8b18f7aff91b7f30cf427a9067df1a12ab641
xroah <396069583@qq.com>
2021-09-23 18:32:32 +0800

    remove nav wrapper

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from "react"
 import classNames from "reap-utils/lib/class-names"
 import PropTypes from "prop-types"
 import {
@@ -8,7 +8,7 @@ import {
     sizes
 } from "../Commons/consts-and-types"
 
-interface PaginationProps extends React.HTMLAttributes<HTMLDivElement> {
+interface PaginationProps extends React.HTMLAttributes<HTMLUListElement> {
     size?: Size
     alignment?: Alignment
 }
@@ -22,18 +22,16 @@ export default function Pagination(
         ...restProps
     }: PaginationProps
 ) {
+    const classes = classNames(
+        "pagination",
+        size && `pagination-${size}`,
+        alignment && `justify-content-${alignment}`
+    )
+
     return (
-        <nav {...restProps}>
-            <ul className={
-                classNames(
-                    "pagination",
-                    size && `pagination-${size}`,
-                    alignment && `justify-content-${alignment}`
-                )
-            }>
-                {children}
-            </ul>
-        </nav>
+        <ul className={classes} {...restProps}>
+            {children}
+        </ul>
     )
 }
 
