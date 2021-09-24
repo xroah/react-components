560765d8634f012fad75490686447c9d076c5c98
xroah <396069583@qq.com>
2021-09-24 19:03:27 +0800

    Breakpoint type

--- components/Layout/Container.tsx
+++ components/Layout/Container.tsx
@@ -1,12 +1,12 @@
 import * as React from "react"
 import {bool, oneOf} from "prop-types"
-import {ValueOf, breakpoints} from "../Commons/consts-and-types"
+import {breakpoints, Breakpoint} from "../Commons/consts-and-types"
 import classNames from "reap-utils/lib/class-names"
 import {getPrefixFunc} from "../Commons/utils"
 
 interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
     fluid?: boolean
-    breakpoint?: ValueOf<typeof breakpoints>
+    breakpoint?: Breakpoint
 }
 
 export default function Container(
