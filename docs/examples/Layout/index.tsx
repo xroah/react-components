baea7436e429c0c7133661166ec972190067a8f7
xroah <396069583@qq.com>
2021-09-27 19:42:29 +0800

    update Layout examples

--- docs/examples/Layout/index.tsx
+++ docs/examples/Layout/index.tsx
@@ -2,12 +2,14 @@ import * as React from "react"
 import Container from "reap-ui/Layout/Container"
 import Col from "reap-ui/Layout/Col"
 import Row from "reap-ui/Layout/Row"
+import Alignment from "reap-ui/Alignment"
 
 export default () => (
     <div className="layout-example">
         <div className="mb-5">Layout</div>
         <Container>
             <Row>
+                <Col>Column</Col>
                 <Col>Column</Col>
                 <Col>Column</Col>
                 <Col>Column</Col>
@@ -26,6 +28,23 @@ export default () => (
                 <Col span={{md: "auto"}}>Variable width content</Col>
                 <Col span={{xs: true, lg: 2}}>3 of 3</Col>
             </Row>
+        </Container>
+        <Container className="row-flex-cols">
+            <Alignment vertical="center">
+                <Row>
+                    <Col>One of three columns</Col>
+                    <Col>One of three columns</Col>
+                    <Col>One of three columns</Col>
+                </Row>
+            </Alignment>
+        </Container>
+        <Container>
+            <Alignment horizontal="center">
+                <Row>
+                    <Col span={4}>One of two columns</Col>
+                    <Col span={4}>One of two columns</Col>
+                </Row>
+            </Alignment>
         </Container>
     </div>
 )
