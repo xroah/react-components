502a4513043933a1971305cea4d781d64680830e
xroah <396069583@qq.com>
2021-10-04 19:37:23 +0800

    Text component

---
+++ components/Utilities/Text.tsx
@@ -0,0 +1,52 @@
+import * as React from "react"
+import {element, number, oneOf} from "prop-types"
+import classNames from "reap-utils/lib/class-names"
+import {TextColor, textColors, ValueOf} from "../Commons/consts-and-types"
+
+const fontWeights = [
+    "bold",
+    "bolder",
+    "normal",
+    "light",
+    "lighter"
+] as const
+const fontStyles = [
+    "italic",
+    "normal"
+] as const
+
+interface CardTextProps {
+    children: React.ReactElement
+    color?: TextColor
+    size?: number
+    weight?: ValueOf<typeof fontWeights>
+    style?: ValueOf<typeof fontStyles>
+}
+
+export default function CardText(
+    {
+        children,
+        color,
+        size,
+        weight,
+        style
+    }: CardTextProps
+) {
+    const c = React.Children.only(children)
+    const classes = classNames(
+        color && `text-${color}`,
+        size && `fs-${size}`,
+        weight && `fw-${weight}`,
+        style && `fst-${style}`
+    )
+
+    return React.cloneElement(c, {className: classes})
+}
+
+CardText.propTypes = {
+    children: element,
+    color: oneOf(textColors),
+    size: number,
+    weight: oneOf(fontWeights),
+    style: oneOf(fontStyles)
+}
