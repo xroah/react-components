d065514c07c92efacc28d3494ea72224c857c801
xroah <396069583@qq.com>
2021-10-30 19:43:19 +0800

    fix errors

--- components/Card/Title.tsx
+++ components/Card/Title.tsx
@@ -1,37 +1,31 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import classNames from "reap-utils/lib/class-names"
-import {Color, colorPropType} from "./Card"
 import {isValidNode} from "../Commons/utils"
+import {TextColor} from "@commons/consts-and-types"
 
 interface CardTitleProps extends React.HTMLAttributes<HTMLDivElement> {
     subTitle?: React.ReactNode
-    subTitleColor?: Color
-    color?: Color
+    subTitleColor?: TextColor
 }
 
 export default function CardTitle(
     {
         subTitle,
         subTitleColor,
-        color,
         children,
         ...restProps
     }: CardTitleProps
 ) {
-    const classes = classNames(
-        "card-title",
-        color && `text-${color}`
-    )
     const subClasses = classNames(
-        "card-title",
+        "card-subtitle",
         subTitleColor && `text-${subTitleColor}`
     )
 
 
     return (
         <div {...restProps}>
-            <h5 className={classes}>
+            <h5 className="card-title">
                 {children}
             </h5>
             {
@@ -46,7 +40,6 @@ export default function CardTitle(
 }
 
 CardTitle.propTypes = {
-    color: colorPropType,
     subTitle: PropTypes.node,
     subTitleColor: PropTypes.node
 }
