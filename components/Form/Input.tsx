d504b9c3510fbe84fdf1de9c2ac1daed44b141e0
xroah <396069583@qq.com>
2021-11-09 20:56:09 +0800

    update Input

--- components/Form/Input.tsx
+++ components/Form/Input.tsx
@@ -1,9 +1,16 @@
 import * as React from "react"
 import {
     Size,
+    sizes,
     ValueOf
 } from "@commons/consts-and-types"
 import classNames from "reap-utils/lib/class-names"
+import {getPrefixFunc} from "@commons/utils"
+import {
+    bool,
+    number,
+    oneOf
+} from "prop-types"
 
 const inputVariants = [
     "input",
@@ -16,4 +23,43 @@ interface InputProps extends Base {
     size?: Size
     htmlSize?: number
     variant?: ValueOf<typeof inputVariants>
+    plain?: boolean
+}
+
+export default function Input(
+    {
+        className,
+        size,
+        htmlSize,
+        variant,
+        plain,
+        ...restProps
+    }: InputProps
+) {
+    const prefix = getPrefixFunc("form-control")
+    const classes = classNames(
+        className,
+        prefix(),
+        size && prefix(size),
+        plain && prefix("plaintext")
+    )
+
+    return React.createElement(
+        variant!,
+        {
+            size: htmlSize,
+            className: classes,
+            ...restProps
+        }
+    )
+}
+
+Input.propTypes = {
+    size: oneOf(sizes),
+    htmlSize: number,
+    variant: oneOf(inputVariants),
+    plain: bool
+}
+Input.defaultProps = {
+    variant: "input"
 }
