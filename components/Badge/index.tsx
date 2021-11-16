97023312358727c7aa9e732f9613214efd0d9815
xroah <396069583@qq.com>
2021-11-16 10:54:23 +0800

    remove indicator and positioned badges

--- components/Badge/index.tsx
+++ components/Badge/index.tsx
@@ -1,15 +1,39 @@
-import Badge from "./Badge"
-import Positioned from "./Positioned"
-import Indicator from "./Indicator"
+import * as React from "react"
+import classNames from "reap-utils/lib/class-names"
+import PropTypes from "prop-types"
+import {ValueOf, Variant} from "../Commons/consts-and-types"
+import {variantPropType} from "@commons/prop-types"
 
-interface BadgeType {
-    Positioned: typeof Positioned
-    Indicator: typeof Indicator
+const colors = ["light", "dark"] as const
+
+export interface BadgeProps extends React.HTMLAttributes<HTMLSpanElement> {
+    variant?: Variant
+    pill?: boolean
+    textColor?: ValueOf<typeof colors>
 }
 
-const _Badge = Badge as (BadgeType & typeof Badge)
+export default function Badge(
+    {
+        className,
+        variant,
+        pill,
+        textColor,
+        ...restProps
+    }: BadgeProps
+) {
+    const classes = classNames(
+        className,
+        "badge",
+        variant && `bg-${variant}`,
+        pill && "rounded-pill",
+        textColor && `text-${textColor}`
+    )
 
-_Badge.Positioned = Positioned
-_Badge.Indicator = Indicator
+    return <span className={classes} {...restProps} />
+}
 
-export default _Badge
+Badge.propTypes = {
+    variant: variantPropType,
+    pill: PropTypes.bool,
+    color: PropTypes.oneOf(colors)
+}
