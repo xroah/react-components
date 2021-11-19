94c204e936f07b7b0707f27f3fc7be294b3d675e
xroah <396069583@qq.com>
2021-11-19 14:59:15 +0800

    delete

--- src/dom/index.ts
+++ src/dom/index.ts
@@ -1,4 +1,3 @@
-export {default as emulateTransitionEnd} from "./emulate-transition-end"
 export {default as getScrollbarWidth} from "./get-scroll-bar-width"
 export {default as getScrollParent} from "./get-scroll-parent"
 export {default as reflow} from "./reflow"
