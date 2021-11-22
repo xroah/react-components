5045f8a8c976e6930b9c5b1ba993a97a14b03928
xroah <396069583@qq.com>
2021-11-22 17:13:45 +0800

    Floating

---
+++ components/Form/Floating.tsx
@@ -0,0 +1,53 @@
+import {element, string} from "prop-types"
+import * as React from "react"
+import classNames from "reap-utils/lib/class-names"
+import {onlyChild} from "../Commons/utils"
+
+let uuid = 0
+
+interface FloatingProps extends React.HTMLAttributes<HTMLElement> {
+    label: string
+    htmlFor?: string
+    children: React.ReactElement
+}
+
+function Floating(
+    {
+        className,
+        label,
+        htmlFor,
+        children,
+        ...restProps
+    }: FloatingProps
+) {
+    const PREFIX = "bs-form-floating-"
+    const classes = classNames(
+        className,
+        "form-floating"
+    )
+    const c = onlyChild(children)
+    let id: string = `${PREFIX}${uuid++}`
+
+    if (htmlFor) {
+        id = htmlFor
+    } else if (c.props.id) {
+        id = c.props.id
+    }
+
+    return (
+        <div className={classes} {...restProps}>
+            {React.cloneElement(c, {id})}
+            <label htmlFor={id}>{label}</label>
+        </div>
+    )
+}
+
+
+Floating.propTypes = {
+    label: string.isRequired,
+    htmlFor: string,
+    children: element.isRequired
+}
+Floating.displayName = "FormFloating"
+
+export default Floating
--- docs/examples/Form/index.tsx
+++ docs/examples/Form/index.tsx
@@ -2,9 +2,11 @@ import * as React from "react"
 import Checkbox from "reap-ui/Form/Checkbox"
 import Radio from "reap-ui/Form/Radio"
 import Switch from "reap-ui/Form/Switch"
+import Floating from "reap-ui/Form/Floating"
+import Input from "reap-ui/Form/Input"
 import Text from "reap-ui/Utilities/Text"
 
-export default() => (
+export default () => (
     <div className="my-3">
         <Text color="success" size={3}>
             <p>Form</p>
@@ -20,6 +22,14 @@ export default() => (
         <div>
             <Switch>Default switch checkbox input</Switch>
             <Switch defaultChecked>Default switch checkbox input</Switch>
+        </div>
+        <div className="my-3">
+            <Floating label="Floating label" htmlFor="floating">
+                <Input
+                    placeholder="哈哈哈"
+                    variant="textarea"
+                    style={{height: 100}}/>
+            </Floating>
         </div>
     </div>
 )
