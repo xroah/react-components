020e4100926b9d3ccb3e1c84e88c530889d67803
xroah <396069583@qq.com>
2022-02-18 10:19:52 +0800

    remove popup, use floating-ui instead

--- demo/components/App.tsx
+++ demo/components/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import Popup from "./Popup"
 import OffCanvas from "./OffCanvas"
 import Modal from "./Modal"
 import Toast from "./Toast"
@@ -9,8 +8,6 @@ import Message from "./Message"
 export default () => (
     <>
         <div style={{height: 1000}}></div>
-        <div className="my-3 text-danger fs-3">Popup</div>
-        <Popup />
         <div className="my-3 text-danger fs-3">OffCanvas</div>
         <OffCanvas />
         <div className="my-3 text-danger fs-3">Modal</div>
