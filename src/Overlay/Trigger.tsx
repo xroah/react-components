79fb86d86198ed9d7113d42f61147b94d6cc4e50
xroah <396069583@qq.com>
2022-04-15 12:55:20 +0800

    class component

--- src/Overlay/Trigger.tsx
+++ src/Overlay/Trigger.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import {omit} from "reap-utils"
 import {
     getNextNodeByRef,
     only,
@@ -9,137 +10,157 @@ import {handleActions} from "../Commons/utils"
 import Overlay, {OverlayCommonProps} from "./Overlay"
 
 export interface TriggerProps extends OverlayCommonProps {
-    overlayRef?: React.RefObject<HTMLElement>
     nodeRef?: React.RefObject<HTMLElement>
     action?: TriggerType | TriggerType[]
     overlay: React.ReactElement
     unmountOnOverlayExit?: boolean
 }
 
-export default function Trigger(
-    {
-        overlay,
-        overlayRef,
-        nodeRef,
-        action,
-        children,
-        unmountOnOverlayExit,
-        ...restProps
-    }: TriggerProps
-) {
-    const child = only(children)
-    const {
-        onClick: cOnClick,
-        onFocus: cOnFocus,
-        onBlur: cOnBlur,
-        onMouseEnter: cOnMouseEnter,
-        onMouseLeave: cOnMouseLeave
-    } = child.props
-    const placeholderRef = React.useRef<HTMLDivElement>(null)
-    const childRef = React.useRef<HTMLElement>(null)
-    const [visible, update] = React.useState(false)
-    const onClick = React.useCallback(
-        (evt: React.MouseEvent) => {
-            cOnClick?.(evt)
-            update(!visible)
-        },
-        [cOnClick, visible]
-    )
-    const onFocus = React.useCallback(
-        (evt: React.FocusEvent) => {
-            cOnFocus?.(evt)
-            update(true)
-        },
-        [cOnFocus]
-    )
-    const onBlur = React.useCallback(
-        (evt: React.FocusEvent) => {
-            cOnBlur?.(evt)
-            update(false)
-        },
-        [cOnBlur]
-    )
-    const onMouseEnter = React.useCallback(
-        (evt: React.MouseEvent) => {
-            cOnMouseEnter?.(evt)
-            update(true)
-        },
-        [cOnMouseEnter]
-    )
-    const onMouseLeave = React.useCallback(
-        (evt: React.MouseEvent) => {
-            cOnMouseLeave?.(evt)
-            update(false)
-        },
-        [cOnMouseLeave]
-    )
-    const getNode = () => {
-        const node = getNextNodeByRef(placeholderRef)
+interface State {
+    visible: Boolean
+}
 
-        if (node) {
-            (childRef as any).current = node
+class Trigger extends React.Component<TriggerProps, State> {
+    static defaultProps = {
+        action: "click"
+    }
+
+    private _placeholderRef = React.createRef<HTMLDivElement>()
+    private _childRef = React.createRef<HTMLElement>()
+
+    constructor(props: TriggerProps) {
+        super(props)
+
+        this.state = {
+            visible: false
         }
     }
-    const callbacks = React.useMemo(
-        () => {
-            const listeners: ElProps = {
-                onClick,
-                onMouseEnter,
-                onMouseLeave,
-                onFocus,
-                onBlur
-            }
-            const ret: ElProps = {}
-            const actions = handleActions(action)
 
-            for (let a of actions) {
-                if (listeners.hasOwnProperty(a)) {
-                    ret[a] = listeners[a]
-                }
+    static getDerivedStateFromProps(
+        nextProps: TriggerProps,
+        nextState: State
+    ) {
+        if ("visible" in nextProps) {
+            return {
+                visible: !!nextProps.visible
             }
+        }
 
-            return ret
-        },
-        [
-            action,
+        return nextState
+    }
+
+    componentDidUpdate() {
+        if (this.props.nodeRef) {
+            return
+        }
+
+        const node = getNextNodeByRef(this._placeholderRef)
+
+        if (node) {
+            (this._childRef as any).current = node
+        }
+    }
+
+    private _getCallbacks() {
+        const {children, action} = this.props
+        const {
+            onClick: cOnClick,
+            onFocus: cOnFocus,
+            onBlur: cOnBlur,
+            onMouseEnter: cOnMouseEnter,
+            onMouseLeave: cOnMouseLeave
+        } = (children as React.ReactElement).props
+        const onClick = (evt: React.MouseEvent) => {
+            cOnClick?.(evt)
+            this.toggle()
+            console.log("ccc")
+        }
+        const onFocus = (evt: React.FocusEvent) => {
+            cOnFocus?.(evt)
+            this.show()
+        }
+        const onBlur = (evt: React.FocusEvent) => {
+            cOnBlur?.(evt)
+            this.hide()
+        }
+        const onMouseEnter = (evt: React.MouseEvent) => {
+            cOnMouseEnter?.(evt)
+            this.show()
+        }
+        const onMouseLeave = (evt: React.MouseEvent) => {
+            cOnMouseLeave?.(evt)
+            this.hide()
+        }
+        const listeners: ElProps = {
             onClick,
             onMouseEnter,
             onMouseLeave,
-            onBlur,
-            onFocus
-        ]
-    )
-    const c = React.cloneElement(
-        child,
-        {
-            ...callbacks
+            onFocus,
+            onBlur
         }
-    )
+        const ret: ElProps = {}
+        const actions = handleActions(action)
 
-    React.useEffect(
-        () => {
-            if (!childRef.current) {
-                getNode()
+        for (let a of actions) {
+            if (listeners.hasOwnProperty(a)) {
+                ret[a] = listeners[a]
             }
-        },
-        [visible]
-    )
+        }
 
-    return (
-        <>
-            {!nodeRef && <Placeholder ref={placeholderRef} />}
-            {c}
-            <Overlay
-                targetRef={childRef}
-                visible={visible}
-                unmountOnExit={unmountOnOverlayExit}
-                {...restProps}>
-                {overlay}
-            </Overlay>
-        </>
-    )
+        return ret
+    }
+
+    toggle() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+
+    show() {
+        this.setState({
+            visible: true
+        })
+    }
+
+    hide() {
+        this.setState({
+            visible: false
+        })
+    }
+
+    render() {
+        const {
+            overlay,
+            nodeRef,
+            children,
+            unmountOnOverlayExit,
+            ...restProps
+        } = this.props
+
+        const child = only(children)
+        const c = React.cloneElement(
+            child,
+            {
+                ...this._getCallbacks()
+            }
+        )
+
+        omit(restProps, ["visible", "action"])
+
+        return (
+            <>
+                {!nodeRef && <Placeholder ref={this._placeholderRef} />}
+                {c}
+                <Overlay
+                    targetRef={nodeRef ? nodeRef : this._childRef}
+                    visible={!!this.state.visible}
+                    unmountOnExit={unmountOnOverlayExit}
+                    {...restProps}>
+                    {overlay}
+                </Overlay>
+            </>
+        )
+    }
 }
 
-Trigger.defaultProps = {
-    action: "click"
-}
+export default Trigger
