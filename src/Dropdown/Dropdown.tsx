52a445d546a10b91311b2035edbd17163ed109a3
xroah <396069583@qq.com>
2022-04-25 13:46:17 +0800

    key down

--- src/Dropdown/Dropdown.tsx
+++ src/Dropdown/Dropdown.tsx
@@ -25,6 +25,10 @@ const Dropdown: React.FunctionComponent<DropdownProps> = (
         () => ref.current?.hide(),
         []
     )
+    const show = React.useCallback(
+        () => ref.current?.show(),
+        []
+    )
     const ctx = React.useMemo(
         () => {
             const value: ContextValue = {close: null}
@@ -36,6 +40,90 @@ const Dropdown: React.FunctionComponent<DropdownProps> = (
             return value
         },
         [autoClose]
+    )
+    const selectMenu = React.useCallback(
+        (next: boolean) => {
+            const {current: el} = overlayRef
+
+            if (!el) {
+                return
+            }
+
+            const SELECTOR = `.dropdown-item:not(.disabled):not(:disabled)`
+            const list = el.querySelectorAll(SELECTOR)
+            const {activeElement} = document
+            const len = list.length
+            let activeIndex = -1
+            let nextActive: Element
+
+            if (activeElement) {
+                for (let i = 0; i < len; i++) {
+                    const item = list[i]
+
+                    if (
+                        item === activeElement ||
+                        item.contains(activeElement)
+                    ) {
+                        activeIndex = i
+
+                        break
+                    }
+                }
+            }
+
+            if (activeIndex === -1) {
+                nextActive = next ? list[0] : list[len - 1]
+            } else {
+                activeIndex += next ? 1 : -1
+                activeIndex = (activeIndex + len) % len
+                nextActive = list[activeIndex]
+            }
+
+            if (nextActive) {
+                (nextActive as HTMLElement).focus()
+            }
+        },
+        []
+    )
+    const handleKeyDown = React.useMemo(
+        () => {
+            // controlled
+            if ("visible" in restProps) {
+                return () => { }
+            }
+
+            return (evt: React.KeyboardEvent) => {
+                const t = evt.target as HTMLElement
+                const isInput = /input|textarea/i.test(t.tagName)
+                const key = evt.key.toLowerCase()
+                const ESC_KEY = "escape"
+                const UP_KEY = "arrowup"
+                const DOWN_KEY = "arrowdown"
+                const keySet = new Set([
+                    ESC_KEY,
+                    UP_KEY,
+                    DOWN_KEY
+                ])
+
+                if (
+                    !keySet.has(key) ||
+                    (isInput && key !== ESC_KEY)
+                ) {
+                    return
+                }
+
+                if (key === ESC_KEY) {
+                    close()
+                } else {
+                    show()
+                    selectMenu(key === DOWN_KEY)
+                }
+
+                evt.preventDefault()
+                evt.stopPropagation()
+            }
+        },
+        [restProps.visible]
     )
 
     omit(
@@ -54,6 +142,8 @@ const Dropdown: React.FunctionComponent<DropdownProps> = (
                 overlayRef={overlayRef}
                 fade={false}
                 closeOnClickOutside={autoClose && autoClose !== "inside"}
+                onTargetKeyDown={handleKeyDown}
+                onKeyDown={handleKeyDown}
                 {...restProps} />
         </DropdownContext.Provider>
     )
