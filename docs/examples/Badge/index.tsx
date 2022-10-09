c3b19f0f0d4692fe40c3dfae9759e3dbb7e86052
xroah <396069583@qq.com>
2022-10-09 18:43:49 +0800

    remove

--- docs/examples/Badge/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import Badge from "reap-ui/Badge"
-
-export default () => (
-    <div>
-        <Badge variant="primary">primary</Badge>
-        <Badge variant="warning" pill textColor="dark">warning</Badge>
-    </div>
-)
