7659589c360dafd739e1cc15e5de1361ba1b37c5
xroah <396069583@qq.com>
2022-11-06 10:30:17 +0800

    no-transition3

---
+++ src/commons/no-transition.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import { EndListenerProps } from "react-transition-group/Transition"
+import { ToggleEvents } from "./types"
+
+type BaseProps = Omit<EndListenerProps<HTMLElement>, "addEndListener">
+
+interface NOTransitionProps extends ToggleEvents, BaseProps {
+    children: React.ReactNode
+}
+
+class NOTransition extends React.Component<NOTransitionProps> {
+    componentDidUpdate(prevProps: NOTransitionProps) {
+        const {
+            onEnter,
+            onEntering,
+            onEntered,
+            onExit,
+            onExiting,
+            onExited,
+            visible,
+            in: _in
+        } = this.props
+
+        if (prevProps.in !== _in) {
+            if (_in) {
+                onEnter?.(false)
+                onEntering?.(false)
+                onEntered?.(false)
+            } else {
+                onExit?.()
+                onExiting?.()
+                onExited?.()
+            }
+        }
+    }
+
+    render() {
+        if (this.props.unmountOnExit) {
+            return null
+        }
+
+        return this.props.children
+    }
+}
+
+export default NOTransition
