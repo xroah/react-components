d902fcad5023e71e86ef3e911dfdee550f7ab170
xroah <396069583@qq.com>
2022-11-07 20:32:19 +0800

    import bootstrap scss

--- docs/index.tsx
+++ docs/index.tsx
@@ -2,7 +2,6 @@ import * as React from "react"
 import { createRoot } from "react-dom/client"
 import App from "./app"
 
-import "bootstrap/dist/css/bootstrap.css"
 import "./styles/index.scss"
 import "../src/styles/index.scss"
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 export default defineConfig({
     plugins: [react()],
     base: "./",
+    resolve: {
+        alias: [
+            {
+                find: /~(.+)/,
+                replacement: path.join(process.cwd(), 'node_modules/$1'),
+            }
+        ]
+    },
     server: {
         open: true,
         port: 8000,
