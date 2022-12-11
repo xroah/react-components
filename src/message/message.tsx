67bda70f2a0d74f7073e5a1919425eb65aed26d6
xroah <396069583@qq.com>
2022-12-11 21:49:44 +0800

    message component

---
+++ src/message/message.tsx
@@ -0,0 +1,104 @@
+import classNames from "classnames";
+import React, { FunctionComponent } from "react"
+import { Transition } from "react-transition-group"
+import Alert, { AlertProps } from "../alert";
+import { ToggleEvents } from "../commons/types";
+
+const DEFAULT_DURATION = 3000
+
+export interface MessageProps extends AlertProps, ToggleEvents {
+    duration?: number
+}
+
+interface State {
+    visible: boolean
+}
+
+class Message extends React.Component<MessageProps, State> {
+    private timer = -1
+
+    constructor(props: MessageProps) {
+        super(props)
+
+        this.state = {
+            visible: true
+        }
+    }
+
+    clearTimeout() {
+        if (this.timer !== -1) {
+            window.clearTimeout(this.timer)
+
+            this.timer = -1
+        }
+    }
+
+    close = () => {
+        this.setState({ visible: false })
+    }
+
+    componentDidMount() {
+        const { duration = DEFAULT_DURATION } = this.props
+
+        if (duration! > 0) {
+            this.timer = window.setTimeout(
+                this.close,
+                duration
+            )
+        }
+    }
+
+    componentWillUnmount() {
+        this.clearTimeout()
+    }
+
+    render() {
+        const {
+            onShow,
+            onShown,
+            className,
+            onHide,
+            onHidden,
+            ...restProps
+        } = this.props
+
+        delete restProps.onClose
+        delete restProps.duration
+
+        return (
+            <Transition
+                in={this.state.visible}
+                timeout={150}
+                onEnter={onShow}
+                onEntered={onShown}
+                onExit={onHide}
+                onExited={onHidden}
+                appear
+                unmountOnExit>
+                {
+                    state => {
+                        const isEnter = (
+                            state === "entering" ||
+                            state === "entered"
+                        )
+                        const classes = classNames(
+                            className,
+                            "r-message-item",
+                            isEnter && "show"
+                        )
+
+                        return (
+                            <div className={classes}>
+                                <Alert
+                                    onClose={this.close}
+                                    {...restProps} />
+                            </div>
+                        )
+                    }
+                }
+            </Transition>
+        )
+    }
+}
+
+export default Message
