39e6cf83b97c1336c38946c5c907856adb810673
xroah <396069583@qq.com>
2023-01-06 21:59:18 +0800

    utils

--- src/commons/utils.ts
+++ src/commons/utils.ts
@@ -5,3 +5,24 @@ export function getZIndex() {
 
     return zIndex
 }
+
+export function noop() {
+    // do nothing
+}
+
+export function omit<T>(
+    o: T,
+    k: keyof T | Array<keyof T>
+): Partial<T> {
+    let keys: Array<keyof T> = []
+
+    if (!Array.isArray(k)) {
+        keys = [k]
+    }
+
+    for (const key of keys) {
+        delete o[key]
+    }
+
+    return o
+}
