4fe4d82732d30d160c6ac6ae7c0a1f955a4b4215
xroah <396069583@qq.com>
2023-01-14 11:05:36 +0800

    fix: omit function cause error if o is undefined or nulll

--- src/commons/utils.ts
+++ src/commons/utils.ts
@@ -10,10 +10,18 @@ export function noop() {
     // do nothing
 }
 
+export function isPlainObject(o: unknown) {
+    return Object.prototype.toString.call(o) === "[object Object]"
+}
+
 export function omit<T>(
     o: T,
     k: keyof T | Array<keyof T>
 ): Partial<T> {
+    if (!isPlainObject(o)) {
+        return o
+    }
+
     let keys: Array<keyof T> = []
 
     if (!Array.isArray(k)) {
