4bc702c8d7f328e69c55330692a516be5e3e2570
xroah <396069583@qq.com>
2023-01-21 19:11:23 +0800

    utils

---
+++ src/components/utils/index.ts
@@ -0,0 +1,43 @@
+let zIndex = 1000
+
+export function getZIndex() {
+    zIndex += 1
+
+    return zIndex
+}
+
+export function noop() {
+    // do nothing
+}
+
+export function isPlainObject(o: unknown) {
+    return Object.prototype.toString.call(o) === "[object Object]"
+}
+
+export function omit<T>(
+    o: T,
+    k: keyof T | Array<keyof T>
+): Partial<T> {
+    if (!isPlainObject(o)) {
+        return o
+    }
+
+    let keys: Array<keyof T> = []
+
+    if (!Array.isArray(k)) {
+        keys = [k]
+    }
+
+    for (const key of keys) {
+        delete o[key]
+    }
+
+    return o
+}
+
+export function classnames(...classes: unknown[]): string {
+    return classes
+        .filter(c => !!c)
+        .map(c => String(c).trim())
+        .join(" ")
+}
