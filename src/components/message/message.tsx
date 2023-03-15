520d82ec47fa908e34c24d2decf53863d152b9ce
xroah <396069583@qq.com>
2023-03-15 22:32:32 +0800

    optimize the transition

--- src/components/message/message.tsx
+++ src/components/message/message.tsx
@@ -1,9 +1,9 @@
-import React from "react"
+import React, { CSSProperties } from "react"
 import { Transition, TransitionStatus } from "react-transition-group"
 import Alert, { AlertProps } from "../basics/alert"
 import { ToggleEvents } from "../commons/types"
 import { number } from "prop-types"
-import { classnames, omit } from "../utils"
+import { omit } from "../utils"
 
 const DEFAULT_DURATION = 3000
 export const WRAPPER_CLASS = "r-message-wrapper"
@@ -15,6 +15,8 @@ export interface MessageProps extends AlertProps, ToggleEvents {
 
 class Message extends React.Component<MessageProps> {
     private timer = -1
+    private ref = React.createRef<HTMLDivElement>()
+
     static propTypes = {
         duration: number
     }
@@ -51,31 +53,46 @@ class Message extends React.Component<MessageProps> {
         const {
             onShow,
             onShown,
-            className,
             onHide,
             onHidden,
             visible,
             ...restProps
         } = this.props
+        const defaultStyle: CSSProperties = {
+            transform: "translateY(-150px)",
+            opacity: 0
+        }
         const render = (s: TransitionStatus) => {
             const show = s === "entering" || s === "entered"
-            const classes = classnames(
-                className,
-                "r-message-item",
-                show && "show"
-            )
+            const style: CSSProperties = {}
+
+            if (show) {
+                style.transform = "none"
+                style.opacity = 1
+                style.height = this.ref.current?.scrollHeight
+            } else if (s === "exiting") {
+                style.height = 0
+                style.opacity = 0
+            }
 
             return (
-                <div className={classes}>
+                <div
+                    ref={this.ref}
+                    style={{
+                        ...defaultStyle,
+                        ...style
+                    }}
+                    className="r-message-item">
                     <Alert onClose={this.close} {...restProps} />
                 </div>
             )
         }
 
         omit(restProps, ["duration", "onClose"])
-        
+
         return (
             <Transition
+                nodeRef={this.ref}
                 in={visible}
                 timeout={200}
                 onEnter={onShow}
