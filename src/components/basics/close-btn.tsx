5da66c2308e98fa9ac04e6b94eb473e05c05c652
xroah <396069583@qq.com>
2023-04-20 21:33:09 +0800

    refactor: close button

--- src/components/basics/close-btn.tsx
+++ src/components/basics/close-btn.tsx
@@ -1,15 +1,20 @@
 import React, { ButtonHTMLAttributes } from "react"
+import { classnames } from "../utils"
+import Close from "../icons/close"
 
 export default function CloseBtn(
     {
         type = "button",
+        className,
         ...restProps
     }: ButtonHTMLAttributes<HTMLButtonElement>
 ) {
     return (
         <button
             type={type}
-            className="btn-close"
-            {...restProps} />
+            className={classnames(className, "btn-close")}
+            {...restProps}>
+            <Close />
+        </button>
     )
 }
