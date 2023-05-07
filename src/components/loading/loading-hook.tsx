cd7ca1b42f480cdc23000a802593215bb03fdb43
xroah <396069583@qq.com>
2023-05-07 09:56:31 +0800

    refactor: revise elements structure

--- src/components/loading/loading-hook.tsx
+++ src/components/loading/loading-hook.tsx
@@ -3,7 +3,7 @@ import { createPortal } from "react-dom"
 import Loading, { LoadingProps } from "./loading"
 import { Options, WRAPPER_CLASS } from "./loading-methods"
 import { HookApi } from "../commons/types"
-import { chainFunction, classnames } from "../utils"
+import { chainFunction } from "../utils"
 
 export function useLoading(): [HookApi<LoadingProps>, ReactNode] {
     const [
@@ -27,27 +27,24 @@ export function useLoading(): [HookApi<LoadingProps>, ReactNode] {
 
         closed.current = true
 
-        setProps(p => ({...p, visible: false}))
+        setProps(p => ({ ...p, visible: false }))
     }
     let el: ReactNode = null
 
     if (props) {
-        const classes = classnames(
-            props.className,
-            WRAPPER_CLASS
-        )
         const handleShow = () => closed.current = false
         const handleHidden = () => setProps(null)
         const newProps: LoadingProps = {
             ...props,
-            className: classes,
             visible: props.visible ?? true,
             onShow: chainFunction(handleShow, props.onShow),
             onHidden: chainFunction(handleHidden, props.onHidden),
             onClose: close
         }
         el = createPortal(
-            <Loading {...newProps} />,
+            <div className={WRAPPER_CLASS}>
+                <Loading {...newProps} />
+            </div>,
             document.body
         )
     }
